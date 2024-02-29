9d883e3dacef8786be0bdc0f3afc65fca5eb1291
Surya S <Sathi220632@exlservice.com>
2024-02-29 13:40:02 +0530

    minor changes

--- frontend/src/components/Conversation.js
+++ frontend/src/components/Conversation.js
@@ -1,21 +1,26 @@
-import { useEffect, useState, useCallback } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { IoIosArrowBack } from "react-icons/io";
-import MessageWrapper from "./Message";
+import { useEffect, useState, useCallback } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import { IoIosArrowBack } from "react-icons/io"
+import MessageWrapper from "./Message"
 
 export default function Conversation() {
   const params =  useParams()
   const navigate = useNavigate()
   const [messages, setMessages] = useState([])
   const [input, setInput] = useState("")
-  const [fulfilled, setFulfilled] = useState("")
+  const [fulfilled, setFulfilled] = useState(false)
 
   const fetchMessages = useCallback(async () => {
     try {
-      const response = await fetch(`http://localhost:8000/conversations/${params.id}`);
+      const response = await fetch(`http://localhost:8000/conversations/${params.id}`)
       const conversation = await response.json()
 
-      setFulfilled(conversation.state)
+      if (conversation.state === "Close") {
+        setFulfilled(true)
+      } else {
+        setFulfilled(false)
+      }
+
       setMessages(conversation.messages)
     } catch (error) {
       console.error('Error fetching messages:', error);
@@ -25,6 +30,8 @@ export default function Conversation() {
 
   useEffect(()=> {
     fetchMessages()
+
+    setInput("Hi")
   },[params.id, fetchMessages])
 
   const handleSend = async (e) => {
@@ -66,7 +73,7 @@ export default function Conversation() {
           </div>
         </div>
       </nav>
-      <div className="row" style={{zIndex: -1, marginTop: '10%', marginBottom: '25%',overflow: 'hidden'}}>
+      <div className="row" style={{zIndex:-1, marginTop:'10%', marginBottom:'25%',overflow:'hidden'}}>
         <div className="d-grid gap-0 p-0">
             {messages?.map(message => (
               <div className={"p-1 m-0"} key={message._id}>
@@ -75,8 +82,12 @@ export default function Conversation() {
             ))}
         </div>
         <div className="font-weight-light">
-            <p style={{color: 'red'}}>{fulfilled==='Close'?"This conversation is closed":null}</p>
-            <p className="text-center" style={{fontSize: '12px', color: 'grey'}}>{fulfilled!=='Close'?"This conversation is in progress":null}</p>
+            <p style={{color: 'red'}}>
+              {fulfilled?"This conversation is closed":null}
+            </p>
+            <p className="text-center" style={{fontSize: '12px', color: 'grey'}}>
+              {fulfilled?null:"This conversation is in progress"}
+            </p>
         </div>
       </div>
       <div className="row position-fixed bottom-0 p-0 m-0" style={{maxHeight: '20%', width: '40%', zIndex: 0}}>
@@ -87,13 +98,13 @@ export default function Conversation() {
             value={input}
             onChange={e => setInput(e.target.value)}
             style={{ resize: 'none', height: 'auto', overflow: 'hidden' }} 
-            disabled={fulfilled==='Close'?true:false}
+            disabled={fulfilled}
           />
           <button 
             type="button" 
             className="btn btn-primary mt-2" 
             onClick={e => handleSend(e)}
-            disabled={fulfilled==='Close'?true:false}
+            disabled={fulfilled}
           >
             Send
           </button>
